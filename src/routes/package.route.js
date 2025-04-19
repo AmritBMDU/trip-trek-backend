c825c594ae4782d60b3554fbf778e2e039fd7659
GauravSharma902 <Digitalgauravflutter@gmail.com>
2025-04-19 11:15:32 +0530

    Update package.route.js

--- src/routes/package.route.js
+++ src/routes/package.route.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const { addPackage, getPackage, putPackage, deletePackage } = require('../controllers/package.controller');
 const { uploadSingle } = require('../middlewares/cloudinary.middleware');
-const PackageModel = require('../models/Package.model');
 const router = express.Router();
 
 router.post('/add-package', uploadSingle("image"), (req, res) => {
@@ -19,14 +18,5 @@ router.put('/put-package/:id',uploadSingle("image"),(req,res)=>{
 router.delete('/delete-package/:id',(req,res)=>{
     deletePackage(req,res)
 });
-
-router.delete('/delete-all-package',async (req,res)=>{
-    try {
-        await PackageModel.deleteMany({})
-        console.log("delete all");
-    } catch (error) {
-        console.log("error hai",error);
-    }
-})
 
 module.exports = router;
